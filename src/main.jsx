12bf1b967f83f064623a7e46d505f20a73fd70fd
TheProjectsX <manvstech2.0@gmail.com>
2024-04-07 23:11:55 +0600

    React Router Dom, Context, Firebase Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// React Router Dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserDataContext from "./context/context.jsx";
+
+// Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: []
+  },
+]);
+
+const [userAuthData, setUserAuthData] = useState(null);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UserDataContext.Provider value={{ userAuthData, setUserAuthData }}>
+      <RouterProvider router={router} />
+    </UserDataContext.Provider>
+  </React.StrictMode>
+);
